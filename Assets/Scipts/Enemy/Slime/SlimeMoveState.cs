49742cb655e1a9cf0589d6132657c4df8ad7ed98
Joel7721 <mensahjl@mail.uc.edu>
2024-09-18 19:01:14 -0400

    Added updates to statemachine

---
+++ Assets/Scipts/Enemy/Slime/SlimeMoveState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeMoveState : EnemyState
+{
+
+    private Enemy_Slime enemy;
+
+    public SlimeMoveState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName, Enemy_Slime _enemy) : base(_enemyBase, _stateMachine, _animBoolName)
+    {
+        this.enemy = _enemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
